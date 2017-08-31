c1f0ae9ea487b4d2e36245fb9197d2d5172b5d5b
luchoman08 <luis.manrique@correounivalle.edu.co>
2017-08-30 21:06:09 -0500

    Static files for heroku

--- www/settings.py
+++ www/settings.py
@@ -129,8 +129,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 ALLOWED_HOSTS = ['ec2-54-149-237-227.us-west-2.compute.amazonaws.com',]
 #Carpeta donde se guardaran los archivos de traduccion
 LOCALE_PATHS = [
