2dd6588556ff60a82f7af6d5bb42f267f697dc4b
luchoman08 <luis.manrique@correounivalle.edu.co>
2017-08-30 19:26:12 -0500

    Unmerge database & settings

--- www/settings.py
+++ www/settings.py
@@ -15,30 +15,7 @@ import os
 from django.conf import settings
 
 
-engines = {
-    'sqlite': 'django.db.backends.sqlite3',
-    'postgresql': 'django.db.backends.postgresql_psycopg2',
-    'mysql': 'django.db.backends.mysql',
-}
 
-
-def config():
-    service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
-    if service_name:
-        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['postgresql'])
-    else:
-        engine = engines['sqlite']
-    name = os.getenv('DATABASE_NAME')
-    if not name and engine == engines['sqlite']:
-        name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
-    return {
-        'ENGINE': engine,
-        'NAME': name,
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
-        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
-    }
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -115,9 +92,9 @@ COMPRESS_ROOT = 'polls/static/'
 #        'PORT': '5432',
 #    }
 #}
-
+from . import database
 DATABASES = {
-    'default': config()
+    'default': database.config()
 }
 
 # Password validation
