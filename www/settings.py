df50c6c4251d6b65b70a2a40de2531abc0ac32e9
luchoman08 <luis.manrique@correounivalle.edu.co>
2017-09-08 19:45:13 -0500

    intento 6 git ignore

--- www/settings.py
+++ www/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
