422830c6df82c23de3b2b161d2ab3745ee89d244
luchoman08 <luis.manrique@correounivalle.edu.co>
2017-09-08 19:44:11 -0500

    git ignore intento  5 + settings para rest framework

--- www/settings.py
+++ www/settings.py
@@ -44,7 +44,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
